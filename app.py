06514cd632794c13bd319f4a84b048c76e8ecf5f
SOMESHWARA P H <someshwarph436@gmail.com>
2025-12-14 16:46:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
 
-st.set_page_config(page_title="Test App")
+st.set_page_config(page_title="Brain Tumor Detection")
 
-st.title("✅ Streamlit is Working")
-st.write("If you see this message, deployment is successful.")
-st.success("Your Streamlit app is running correctly!")
+st.title("🧠 Brain Tumor Detection")
+st.write("Upload a brain MRI image to detect tumor.")
 
+@st.cache_resource
+def load_my_model():
+    return load_model("model.h5")
 
+model = load_my_model()
+
+uploaded_file = st.file_uploader(
+    "Choose an MRI image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image", use_column_width=True)
