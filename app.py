07c274f8152178b503b8fb365ea3e1836757703d
Someshwar <someshwarph@gmail.com>
2025-12-14 22:31:09 +0530

    Add Streamlit app with ONNX model using Git LFS

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Brain Tumor App")
+
+st.title("Brain Tumor Detection")
+st.success("✅ Streamlit is running correctly")
+st.write("If you see this, deployment is successful.")
