b5675754339e4ca1088979ab39d9388dbc642264
SOMESHWARA P H <someshwarph436@gmail.com>
2025-12-14 16:44:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 import streamlit as st
+
+st.set_page_config(page_title="Test App")
+
+st.title("✅ Streamlit is Working")
+st.write("If you see this message, deployment is successful.")
+st.success("Your Streamlit app is running correctly!")
+
+
